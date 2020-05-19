fa67bfa866fb28762bfaae27f73684b58ba17ba6
Melody <reid.eastin@gmail.com>
2020-05-19 16:17:37 -0500

    I made a enemy prefab and enemy script
    The script for the enemy movement needs more work but it will chase the target.

---
+++ Assets/Scrips/EnemyMovementBehavior.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovementBehavior : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent agent;
+
+    public int Timer;
+    public float JumpSpeed;
+    private float _oldspeed;
+    private float _timer;
+    [System.NonSerialized]public bool _isstoped;
+    Random rand;
+    public Material Green;
+    public Material Red;
+    private MeshRenderer mesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        mesh = gameObject.GetComponent<MeshRenderer>();
+        mesh.material = Green;
+        _oldspeed = agent.speed;
+        _timer = Timer;
+        _isstoped = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.position;
+
+        if (_isstoped == true)
+        {
+            _timer -= Time.deltaTime;
+
+            if (_timer <= 0.0f)
+            {
+                agent.speed = JumpSpeed;
+                agent.isStopped = false;
+                _timer = Timer;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        agent.isStopped = true;
+        _isstoped = true;
+        Debug.LogWarning("Hi");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        agent.isStopped = false;
+        _timer = Timer;
+        agent.speed = _oldspeed;
+    }
+}

1646ff0663a9f66a3de6ac98ef44763b49b8011f
Melody <reid.eastin@gmail.com>
2020-06-08 10:17:52 -0500

    I removed a component on the goblin prefab
    I also made some changes to the salamander

---
+++ Assets/Scripts/SalamanderMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SalamanderMovement : EnemyBehavior
+{
+    private Transform target;
+    private NavMeshAgent Salamander;
+    private Vector3 JumpBackPosition;
+    private int _oldSpeed;
+    private bool _jumpback;
+    private bool hasTarget;
+
+    public bool TestJumpBack;
+    public float JumpBackTimer;
+    public float JumpBackSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        Salamander = gameObject.GetComponent<NavMeshAgent>();
+        _jumpback = false;
+        hasTarget = false;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_jumpback)
+        {
+            Salamander.destination = target.position;
+        }
+        if(_jumpback)
+        {
+            JumpBack();
+        }
+        if(TestJumpBack)
+        {
+            TestJumpBack = false;
+            _jumpback = true;
+        }
+    }
+
+    public void JumpBack()
+    {
+
+    if(hasTarget)
+        {
+            
+        }
+       
+    }
+
+    public override void Die()
+    {
+        base.Die();
+    }
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+    }
+}

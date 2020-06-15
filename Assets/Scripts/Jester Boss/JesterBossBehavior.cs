03b16d1b2f36a55511156fa76cee3f632953bd5c
SethSylvester <54289099+SethSylvester@users.noreply.github.com>
2020-06-15 14:55:06 -0500

    Update
    -Added the KnifeFork attack to the Jester Boss

--- Assets/Scripts/Jester Boss/JesterBossBehavior.cs
+++ Assets/Scripts/Jester Boss/JesterBossBehavior.cs
@@ -24,6 +24,28 @@ public class JesterBossBehavior : EnemyBehavior
     //The character controller
     private CharacterController _controller;
 
+    private Attack currentAttack = new Attack();
+
+    public float timeDefault = 1.5f;
+    float time;
+
+    [SerializeField]
+    GameObject projectile1;
+    [SerializeField]
+    GameObject projectile2;
+    [SerializeField]
+    GameObject projectile3;
+    [SerializeField]
+    GameObject projectile4;
+
+    bool active1 = false;
+    bool active2 = false;
+    bool active3 = false;
+    bool active4 = false;
+
+    public bool returning = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,12 +56,49 @@ public class JesterBossBehavior : EnemyBehavior
         _gravity = gravityDefault;
         //Calculate the Diagonal speed
         _diagonalSpeed = Mathf.Sqrt((speed * speed) + (speed * speed)) / 2;
+
+        time = timeDefault;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Gravity();
+        //KnifeFork();
+        //Gravity();
+    }
+    
+    void KnifeFork()
+    {
+        //Tick down the timer
+        time -= Time.deltaTime;
+
+        //if all are active and ready to return
+        if (active1 && active2 && active3 && active4 && time <= 0)
+        { returning = true; }
+
+        else if (!active1)
+        {
+            projectile1.SetActive(true);
+            active1 = true;
+        }
+        else if (!active2 && time <= 0)
+        {
+            projectile2.SetActive(true);
+            active2 = true;
+            time = timeDefault;
+        }
+        else if (!active3 && time <= 0)
+        {
+            projectile3.SetActive(true);
+            active3 = true;
+            time = timeDefault;
+        }
+        else if (!active4 && time <= 0)
+        {
+            projectile4.SetActive(true);
+            active4 = true;
+            time = timeDefault;
+        }
     }
 
     private void Gravity()

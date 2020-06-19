b79025f6a7e99b9fc9d18e188e79c12483258e94
Melody <reid.eastin@gmail.com>
2020-06-19 13:13:23 -0500

    I added a timer and speed variable to the fire

--- Assets/Scripts/SalamanderFireBehavior.cs
+++ Assets/Scripts/SalamanderFireBehavior.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 public class SalamanderFireBehavior : MonoBehaviour
 {
     private CharacterController fire;
+    private MeshRenderer Mesh;
     private Vector3 Forward;
+
+    private float _timer;
+    public float Timer;
+
     public int Damage;
+    public int Speed;
 
     // Start is called before the first frame update
     void Start()
     {
         fire = gameObject.GetComponent<CharacterController>();
         Forward = gameObject.transform.forward.normalized;
-        Forward *= 25;
+        Forward *= Speed;
+        _timer = Timer;
+        Mesh = gameObject.GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         fire.Move(Forward * Time.deltaTime);
+        if(_timer <= 0)
+        {
+            Mesh.enabled = true;
+        }
+        else
+        {
+            _timer -= Time.deltaTime;
+        }
     }
     void OnTriggerEnter(Collider other)
     {

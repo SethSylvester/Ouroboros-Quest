1416fa299fb74d0f9cacaeb234a8bfa1e62f920a
SethSylvester <54289099+SethSylvester@users.noreply.github.com>
2020-06-22 13:10:25 -0500

    Update
    -Created Main Menu
    -Updated Player Prefab
    -Created Start Button Script
    -Added Navigate Menu Behavior
    -Added Start Button
    -Added Options Menu
    -Added Exit Button
    -Added Video Button
    -Added Volume Slider

---
+++ Assets/Scripts/Menu/NavigateMenuBehavior.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//The behavior class for the start button of the main menu
+public class NavigateMenuBehavior : MonoBehaviour
+{
+    //0 Main Menu
+    public List<GameObject> Menus;
+
+    [SerializeField]
+    Slider volume;
+
+    public void MainMenu()
+    {
+        //Turn on Main Menu
+        Menus[0].SetActive(true);
+        //Turn off Options
+        Menus[1].SetActive(false);
+    }
+
+    public void OptionsMenu()
+    {
+        //Turn off Main Menu
+        Menus[0].SetActive(false);
+        //Turn on Options
+        Menus[1].SetActive(true);
+        //Turn off Resolution and Volume
+        Menus[2].SetActive(false);
+        Menus[3].SetActive(false);
+    }
+
+    public void ResolutionMenu()
+    {
+        //Turn off Options
+        Menus[0].SetActive(false);
+        //Turn on Resolution
+        Menus[2].SetActive(true);
+    }
+
+    public void VolumeMenu()
+    {
+        //Turn off Options
+        Menus[0].SetActive(false);
+        //Turn on Resolution
+        Menus[3].SetActive(true);
+    }
+
+    public void SetResolution()
+    {
+
+    }
+
+    public void SetVolume()
+    {
+        AudioListener.volume = volume.value;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Menu/StartButtonBehavior.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//The behavior class for the start button of the main menu
+public class StartButtonBehavior : MonoBehaviour
+{
+    public void StartGame()
+    { SceneManager.LoadScene(1); }
+}

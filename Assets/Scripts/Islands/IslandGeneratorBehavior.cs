b97d965de1229286d12a3ce31738c46564c2d696
SethSylvester <54289099+SethSylvester@users.noreply.github.com>
2020-06-24 16:32:36 -0500

    Update
    Added Randomly generated bushes to the island
    -Bushes now slow the player down
    -Disabled the custom diagonal movement script.

--- Assets/Scripts/Islands/IslandGeneratorBehavior.cs
+++ Assets/Scripts/Islands/IslandGeneratorBehavior.cs
@@ -4,35 +4,57 @@ using UnityEngine;
 
 public class IslandGeneratorBehavior : MonoBehaviour
 {
-    private islandTypes biome;
+    public List<Transform> spawnLocations;
+
+    [SerializeField]
+    private GameObject largeTree;
+    [SerializeField]
+    private GameObject smallTree;
+    [SerializeField]
+    private GameObject bush;
+
+    private IslandTypes biome;
     private Color32 groundColor;
 
     private Color32 DarkGreen = new Color32(18, 178, 25, 1);
-
     [SerializeField]
-    GameObject islandFloor;
+    private GameObject islandFloor;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        biome = (islandTypes)Random.Range(1, 3);
+        biome = (IslandTypes)Random.Range(1, 3);
         GenerateGrassLands();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    void GenerateGrassLands()
+    private void GenerateGrassLands()
     {
         groundColor = DarkGreen;
 
-        islandFloor.GetComponent<Renderer>().material.color = DarkGreen;
+        islandFloor.GetComponent<Renderer>().material.color = groundColor;
+
+        float rng;
+        float rngScale;
+
+        for (int i = 0; i < spawnLocations.Count; i++)
+        {
+            rng = Random.Range(1, 4);
+            GameObject enviromentObject;
+
+            //One in three chance to spawn a bush.
+            if (rng == 1)
+            {
+                enviromentObject = Instantiate(bush);
+                enviromentObject.transform.position = spawnLocations[i].transform.position;
+                //Makes it a little bigger depending on the number generated
+                rngScale = (Random.Range(5, 7)/2.5f);
+                enviromentObject.transform.localScale = new Vector3(rngScale, rngScale, rngScale);
+            }
+        }
     }
 }
 
-public enum islandTypes
+public enum IslandTypes
 {
     forest,
     grasslands

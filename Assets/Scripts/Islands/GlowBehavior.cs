2d8ab0bdc9fd52b691bf9a04680ba3c3aaa26e90
SethSylvester <54289099+SethSylvester@users.noreply.github.com>
2020-06-24 11:07:06 -0500

    Update
    -Made the island wider

---
+++ Assets/Scripts/Islands/GlowBehavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowBehavior : MonoBehaviour
+{
+    //If raycasting over the teleporter
+    //Set the material to a glowy texture
+
+
+}

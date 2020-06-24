0860ebd425dff76243ee03bfa1bb15bfa0fc74c0
Ryan LaCombe <ryanalacombe@gmail.com>
2020-06-24 11:17:30 -0500

    Finished the item randomizer

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -5,7 +5,10 @@ using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
-    public GameObject itemDropped;
+    public GameObject itemDropped1;
+    public GameObject itemDropped2;
+    public GameObject itemDropped3;
+    public GameObject itemDropped4;
     public bool isDropped = false;
 
     [SerializeField]
@@ -16,7 +19,7 @@ public class EnemyBehavior : MonoBehaviour
 
     protected NavMeshAgent agent;
 
-    protected int randMax = 20;
+    protected int randMax = 100;
     protected int randMin = 0;
 
     // Start is called before the first frame update
@@ -53,13 +56,33 @@ public class EnemyBehavior : MonoBehaviour
         UnityEngine.Object.Destroy(gameObject, 3);
     }
 
+    //Function for dropping items for each enemy
     void ItemDrop()
     {
         Vector3 itemPosition = agent.transform.position;
         //Creates the random number to determine drops
         int randItem = Random.Range(randMin, randMax);
 
-        GameObject itemDrop = Instantiate(itemDropped, itemPosition, new Quaternion());
+        if (randItem >= 0 && randItem < 30)
+        {
+            GameObject itemDrop = Instantiate(itemDropped1, itemPosition, new Quaternion());
+        }
+        else if (randItem >= 30 && randItem < 55)
+        {
+            GameObject itemDrop = Instantiate(itemDropped2, itemPosition, new Quaternion());
+        }
+        else if (randItem >= 55 && randItem < 75)
+        {
+            GameObject itemDrop = Instantiate(itemDropped3, itemPosition, new Quaternion());
+        }
+        else if (randItem >= 75 && randItem < 95)
+        {
+            GameObject itemDrop = Instantiate(itemDropped4, itemPosition, new Quaternion());
+        }
+        else if (randItem >= 95 && randItem < 100)
+        {
+            return;
+        }
 
         isDropped = true;
     }

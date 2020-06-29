f64fdc40a8945af6fce804d5658113bde28172fb
Melody <reid.eastin@gmail.com>
2020-06-29 16:06:28 -0500

    I made changes to slime and Salamander to fix issues
    PlayerAttackColliderBahavior
    I changed the way it checks if it is the right trigger to fix a issue
    
    Slime and salamander prefab
    I gave them both a hitbox object that has a tag so the player knows if they are hitting the right trigger

---
+++ Assets/Scripts/SlimeJumpAttackHitboxBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeJumpAttackHitboxBehavior : MonoBehaviour
+{
+    private SlimeBehavior Slime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Slime = gameObject.GetComponentInParent<SlimeBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //This checks if the trigger is the trigger on the player
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Slime.BeginJumpAttack();
+        }
+    }
+}
